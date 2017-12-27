6a9a87d149110547426815db2c4c23a5b7f8af47
YuDe <davisfreeman1015@gmail.com>
2017-12-27 11:22:41 +0800

    Update the master info

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='sneak', 
-	version='0.0.1', 
+	# version='0.0.8', # For test
+	version='0.1.0',
 	description='Sneak is a URL transfer tool based on Tor technology.', 
 	url='https://github.com/yudazilian/Sneak',
 	author='yd',
 	author_email='davisfreeman1015@gmail.com',
 	license='Apache 2.0',
-	packages=find_packages('sneak', exclude=['test_*', 'tor_*']),
+	packages=find_packages(),
 	install_requires=[
 		'stem',
 		'pycurl'
