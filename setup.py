53a59e189a48b3fe57ccb454917326dbac90bf14
YuDe <davisfreeman1015@gmail.com>
2017-12-29 12:00:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(name='sneak', 
-	# version='0.0.8', # For test
 	version='0.1.1',
 	description='Sneak is a URL transfer tool based on Tor technology.', 
 	url='https://github.com/yudazilian/Sneak',
